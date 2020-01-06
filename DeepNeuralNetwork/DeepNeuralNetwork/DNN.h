853be03f40f94a48a49f1cc0416b96ba1f2c3053
Melih Yılmaz ÖGÜTCEN <melih.ogutcen@gmail.com>
2020-01-06 15:31:26 +0300

    3rd commit

---
+++ DeepNeuralNetwork/DeepNeuralNetwork/DNN.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Eigen/Core>
+#include "Config.h"
+#include "RNG.h"
+#include "Layer.h"
+#include "Layer/FullyConnected.h"
+#include "Layer/Convolutional.h"
+#include "Layer/Pooling.h"
+
+
+
